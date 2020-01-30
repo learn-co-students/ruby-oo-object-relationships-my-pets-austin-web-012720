7ae2c29319267525509625d8b9c66f46f162ded7
GoTo15 <github email address>
2020-01-30 08:47:08 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+
+    @@all << self
+  end
+
+  # Class Methods
+  def self.all
+    @@all
+  end
+
+  def self.count
+    all.size
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+  
+  # Instance Methods
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def cats
+    Cat.all.select{|x| x.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|x| x.owner == self}
+  end
+
+  def feed_cats
+    self.cats.each{|x| x.mood = "happy"}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def sell_pets
+    self.dogs.each do |x| 
+      x.mood = "nervous"
+      x.owner = nil
+    end
+
+    self.cats.each do |x| 
+      x.mood = "nervous"
+      x.owner = nil
+    end
+  end
+
+  def walk_dogs
+    self.dogs.each{|x| x.mood = "happy"}
+  end
+
 end
