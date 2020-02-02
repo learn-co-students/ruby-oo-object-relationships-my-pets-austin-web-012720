6db6c7ba2ae65bfe91a8257474c2aeffe210db7e
jbshipman <jbshipman@icloud.com>
2020-02-02 09:48:55 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,70 @@
+# Owner class has many for Cat and Dog classes
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def self.all
+    @@all
+  end 
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end 
+
+  def say_species
+    "I am a #{@species}."
+  end 
+
+  def self.count
+    self.all.size
+  end 
+
+  def self.reset_all
+    self.all.clear
+  end 
+
+  def cats
+    # collect all cats associated by owner
+    # select through Cat all array
+    # filter for current owner (or self)
+    Cat.all.select { |cat| cat.owner == self }
+  end 
+
+  def dogs
+    Dog.all.select { |dog| dog.owner == self }
+  end 
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end 
+
+  def walk_dogs
+    self.dogs.each { |dog| dog.mood = "happy" }
+  end 
+  
+  def feed_cats
+    self.cats.each { |cat| cat.mood = "happy" }
+  end 
+
+  def sell_pets
+    pets = self.cats + self.dogs
+
+    pets.each do |sell|
+      sell.mood = "nervous"
+      sell.owner = nil
+    end 
+  end 
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
+
 end
